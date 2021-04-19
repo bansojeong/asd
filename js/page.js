371bacf39469801971a3e616321b264641f82c39
alswjd1125 <alslal1125@naver.com>
2021-04-19 10:27:57 +0900

    04.19
    04.19

--- js/page.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('#fullpage').fullpage({
-        //options here
-        // anchors:["sec01","sec02","sec03","sec04","sec05"],
-		autoScrolling:true,
-        scrollHorizontally: true
-	    });
-    });
